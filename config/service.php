59dc59df5d8255dff4fc8acf452679639e5629a6
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2024-12-10 16:17:16 +0100

    Initial commit

---
+++ config/service.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright 2024 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 10/12/2024, 16:15
+ *
+ * @project Idm Template Bundle
+ * @see     https://github.com/idmarinas/idm-template-bundle
+ *
+ * @file    service.php
+ * @date    10/12/2024
+ * @time    16:15
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+return function (ContainerConfigurator $container) {
+	// @formatter:off
+	$container->services()
+
+	;
+	// @formatter::on
+};
