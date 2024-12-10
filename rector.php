aa3578cf67987a9238d3819758aef59692b9dce2
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2024-12-10 14:20:48 +0100

    Initial commit

---
+++ rector.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Symfony\Set\SymfonySetList;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+
+return RectorConfig::configure()
+	->withPaths([
+		__DIR__ . '/src',
+		__DIR__ . '/tests',
+	])
+	->withPhpSets(php82: true)
+	->withPreparedSets(
+		deadCode           : true,
+		codeQuality        : true,
+		codingStyle        : true,
+		doctrineCodeQuality: true,
+		symfonyCodeQuality : true,
+		symfonyConfigs     : true,
+		twig               : true
+	)
+	->withImportNames(removeUnusedImports: true)
+	->withTypeCoverageLevel(0)
+	->withSets([
+		SymfonySetList::SYMFONY_64,
+		SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
+	])
+	->withRules([AddVoidReturnTypeWhereNoReturnRector::class])
+;
