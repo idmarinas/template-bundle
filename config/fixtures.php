bcd14a30929eb0eca0a9d786fe2a7bcb08615f4f
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2024-12-20 17:49:30 +0100

    First commit

---
+++ config/fixtures.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Copyright 2024 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 20/12/2024, 17:49
+ *
+ * @project IDMarinas Template Bundle
+ * @see     https://github.com/idmarinas/idm-template-bundle
+ *
+ * @file    fixtures.php
+ * @date    20/12/2024
+ * @time    17:49
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.1.0
+ */
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $container) {
+	// @formatter:off
+	$container
+		->services()
+			->load('DataFixtures\\', dirname(__DIR__) . '/fixtures')
+			->public()
+			->autowire()
+			->autoconfigure()
+	;
+	// @formatter:on
+};
---
+++ config/routes.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Copyright 2024 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 20/12/2024, 17:48
+ *
+ * @project IDMarinas Template Bundle
+ * @see     https://github.com/idmarinas/idm-template-bundle
+ *
+ * @file    routes.php
+ * @date    20/12/2024
+ * @time    17:48
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.1.0
+ */
+
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+
+return function (RoutingConfigurator $routes) {
+	// @formatter:off
+
+	// @formatter:on
+};
