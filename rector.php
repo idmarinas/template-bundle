386ea68b1085d05e1e5562ecef0d4be9b9d04340
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-02 22:23:33 +0100

    feat(config): add additional paths to Rector configuration
    - Include app/src, factories, and fixtures directories for analysis.

--- rector.php
+++ rector.php
@@ -1,4 +1,21 @@
 <?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 02/01/2025, 22:23
+ *
+ * @project IDMarinas Template Bundle
+ * @see     https://github.com/idmarinas/idm-template-bundle
+ *
+ * @file    rector.php
+ * @date    02/01/2025
+ * @time    22:23
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
 
 declare(strict_types=1);
 
@@ -8,6 +25,9 @@ use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRect
 
 return RectorConfig::configure()
 	->withPaths([
+		__DIR__ . '/app/src',
+		__DIR__ . '/factories',
+		__DIR__ . '/fixtures',
 		__DIR__ . '/src',
 		__DIR__ . '/tests',
 	])
