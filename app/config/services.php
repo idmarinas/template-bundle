92f9c4f7457b053f6238422f865cf5eb9f0d2c1d
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-03 19:48:55 +0100

    refactor(config): rename service.php to services.php

--- app/config/services.php
+++ app/config/services.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Copyright 2024 (C) IDMarinas - All Rights Reserved
+ * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 30/12/2024, 17:53
+ * Last modified by "IDMarinas" on 03/01/2025, 19:48
  *
  * @project IDMarinas Template Bundle
  * @see     https://github.com/idmarinas/idm-template-bundle
  *
- * @file    service.php
+ * @file    services.php
  * @date    30/12/2024
  * @time    17:53
  *
