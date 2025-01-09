a9b9ecb2a0b87258cd13df469b51ab59a5a0b75e
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-09 21:46:07 +0100

    feat(bundles): add MakerBundle to the application configuration

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Copyright 2024 (C) IDMarinas - All Rights Reserved
+ * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 30/12/2024, 17:53
+ * Last modified by "IDMarinas" on 09/01/2025, 21:46
  *
  * @project IDMarinas Template Bundle
  * @see     https://github.com/idmarinas/idm-template-bundle
@@ -22,6 +22,7 @@ use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle;
 use Idm\Bundle\Template\IdmTemplateBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\MakerBundle\MakerBundle;
 use Zenstruck\Foundry\ZenstruckFoundryBundle;
 
 return [
@@ -30,6 +31,7 @@ return [
 	IdmTemplateBundle::class      => ['all' => true],
 
 	// Dev-Test Bundles
+	MakerBundle::class            => ['all' => true],
 	DoctrineFixturesBundle::class => ['all' => true],
 	DAMADoctrineTestBundle::class => ['all' => true],
 	ZenstruckFoundryBundle::class => ['all' => true],
