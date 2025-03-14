5a5e2d9e6b7a46f36ab78c27a76d210c1e1ba9cf
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-14 23:33:26 +0100

    refactor(tests): update IntegrationTest to use Kernel class
    - Replaced the custom ExtensionTestingKernel with the Symfony Kernel.
    - Updated the configuration loading to utilize the idm_advertising.php file.
    - Removed the unnecessary ExtensionTestingKernel class.

--- tests/Twig/Extension/IntegrationTest.php
+++ tests/Twig/Extension/IntegrationTest.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2021-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 14/03/2025, 23:21
+ * Last modified by "IDMarinas" on 14/03/2025, 23:27
  *
  * @project IDMarinas Template Bundle
  * @see     https://github.com/idmarinas/idm-template-bundle
@@ -19,9 +19,8 @@
 
 namespace Idm\Bundle\Template\Tests\Twig\Extension;
 
-use Symfony\Component\Config\Loader\LoaderInterface;
+use App\Kernel;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpKernel\Kernel;
 use Twig\Test\IntegrationTestCase;
 
 /**
@@ -43,24 +42,10 @@ final class IntegrationTest extends IntegrationTestCase
 
 	protected function getContainer (): ContainerInterface
 	{
-		$kernel = new ExtensionTestingKernel();
+		$kernel = new Kernel('test', true);
+		$kernel->addExtraConfig(dirname(__DIR__, 2) . '/config/idm_advertising.php');
 		$kernel->boot();
 
 		return $kernel->getContainer();
 	}
-}
-
-class ExtensionTestingKernel extends Kernel
-{
-	public function __construct ()
-	{
-		parent::__construct('test', true);
-	}
-
-	public function registerBundles (): iterable
-	{
-		return [];
-	}
-
-	public function registerContainerConfiguration (LoaderInterface $loader): void {}
 }
