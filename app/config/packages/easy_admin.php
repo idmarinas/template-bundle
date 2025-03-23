217e0929546ad9bfd057bc9667d20827c5982613
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-23 21:22:11 +0100

    feat(easy_admin): add initial configuration for EasyAdmin integration

---
+++ app/config/packages/easy_admin.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 23/03/2025, 21:21
+ *
+ * @project IDMarinas Template Bundle
+ * @see     https://github.com/idmarinas/idm-template-bundle
+ *
+ * @file    easy_admin.php
+ * @date    23/03/2025
+ * @time    21:18
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+return static function (ContainerConfigurator $container) {
+	$container->extension('framework', [
+		'forms'  => true,
+		'assets' => true,
+		'router' => [
+			'utf8' => true,
+		],
+	]);
+};
