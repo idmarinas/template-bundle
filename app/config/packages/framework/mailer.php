d30cc83bb4e6cc52a3e1363928692a74edf63a65
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-03-24 23:40:59 +0100

    refactor(mailer): update mailer configuration to use FrameworkConfig

--- app/config/packages/framework/mailer.php
+++ app/config/packages/framework/mailer.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Copyright 2024 (C) IDMarinas - All Rights Reserved
+ * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 30/12/2024, 17:53
+ * Last modified by "IDMarinas" on 24/03/2025, 17:34
  *
  * @project IDMarinas Template Bundle
  * @see     https://github.com/idmarinas/idm-template-bundle
@@ -19,16 +19,13 @@
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-return static function (ContainerConfigurator $container) {
-	$container->extension('framework', [
-		'mailer' => [
-			'dsn'      => $_ENV['MAILER_DSN'] ?? 'null://null',
-			'envelope' => [
-				'sender' => 'idm_bundle@test.bundle',
-			],
-			'headers'  => [
-				'From' => 'IDMarinas Template Bundle <idm_bundle@test.bundle>',
-			],
-		],
-	]);
+use Symfony\Config\FrameworkConfig;
+
+return static function (FrameworkConfig $config) {
+    $config
+        ->mailer()
+        ->dsn($_ENV['MAILER_DSN'] ?? 'null://null')
+        ->envelope()->sender('idm_bundle@test.bundle')
+    ;
+    $config->mailer()->header('From', 'IDMarinas Template Bundle <idm_bundle@test.bundle>');
 };
