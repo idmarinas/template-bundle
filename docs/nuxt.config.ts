0892da03ea2ee2d0905f09dba50867c2c1d60507
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-12-30 20:45:22 +0100

    chore(docs): add Nuxt configuration and dependencies for documentation setup

---
+++ docs/nuxt.config.ts
@@ -0,0 +1,8 @@
+export default defineNuxtConfig({
+	extends: ['github:idmarinas/nuxt-layers/docs-bundle#master', 'docus'],
+	docsBundle: {
+		package_name: 'idmarinas/template-bundle',
+		description: 'Short description for your Symfony Bundle',
+		versions: [/* new versions here. "major.minor" avoid patch versions */ '1.0']
+	}
+})
