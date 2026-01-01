b02c493d7c35b4e3440eabd2598a8d21f8360cf8
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-01 20:58:55 +0100

    chore(ci): simplify base URL handling in docs workflow and update Nuxt config

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -4,5 +4,9 @@ export default defineNuxtConfig({
 		package_name: 'idmarinas/template-bundle',
 		description: 'Short description for your Symfony Bundle',
 		versions: [/* new versions here. "major.minor" avoid patch versions */ '1.0']
+	},
+	ssr: false,
+	app: {
+		baseURL: '/template-bundle/'
 	}
 })
