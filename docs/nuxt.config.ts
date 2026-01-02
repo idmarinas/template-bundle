0aa2e202b6b024831615efaad7299b59c04c34c1
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-02 15:32:04 +0100

    chore(config): update Nuxt config to adjust baseURL and add dynamic LLMS domain for production

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,16 +1,26 @@
 export default defineNuxtConfig({
 	extends: ['github:idmarinas/nuxt-layers/docs-bundle#master', 'docus'],
 	docsBundle: {
-		package_name: 'idmarinas/template-bundle',
+		// package_name: 'idmarinas/template-bundle',
 		description: 'Short description for your Symfony Bundle',
 		versions: [
 			/* new versions here. "major.minor" avoid patch versions */
 			'1.0'
 		]
 	},
+	app: {
+		baseURL: '/'
+	},
 	$production: {
 		app: {
 			baseURL: '/template-bundle/'
+		},
+		llms: {
+			domain: (() => {
+				// const config = useRuntimeConfig()
+				// const docs = config.docsBundle
+				return `https://idmarinas.github.io/template-bundle`
+			})()
 		}
-	}
+	},
 })
