d51679dcd5fbda8124bce12f5292824d818a5d0a
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-02 12:36:04 +0100

    chore(config): add dynamic app baseURL handling in Nuxt config for production environment

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -3,6 +3,12 @@ export default defineNuxtConfig({
 	docsBundle: {
 		package_name: 'idmarinas/template-bundle',
 		description: 'Short description for your Symfony Bundle',
-		versions: [/* new versions here. "major.minor" avoid patch versions */ '1.0']
+		versions: [
+			/* new versions here. "major.minor" avoid patch versions */
+			'1.0'
+		]
+	},
+	app: {
+		baseURL: 'production' === process.env.NODE_ENV ? '/template-bundle/' : '/'
 	}
 })
