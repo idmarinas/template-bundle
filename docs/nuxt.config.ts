aa6f9ac77c07d2646d486c737ee6ead7488815d2
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-02 16:44:34 +0100

    chore(config): clean up docs configuration and add missing package description

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,20 +1,7 @@
 export default defineNuxtConfig({
 	extends: ['github:idmarinas/nuxt-layers/docs-bundle#master', 'docus'],
-	docsBundle: {
-		// package_name: 'idmarinas/template-bundle',
-		description: 'Short description for your Symfony Bundle',
-		versions: [
-			/* new versions here. "major.minor" avoid patch versions */
-			'1.0'
-		]
-	},
-	// app: {
-	// 	baseURL: '/'
-	// },
+	docsBundle: {},
 	$production: {
-		// app: {
-		// 	baseURL: '/template-bundle/'
-		// },
 		llms: {
 			domain: 'https://idmarinas.github.io/template-bundle'
 		}
