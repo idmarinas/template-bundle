1e95eb029a90c257885ba3317f6d0e1bc333227a
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-06 21:29:37 +0200

    chore(docs): add social links to docsBundle configuration in nuxt.config.ts

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,6 +1,15 @@
 export default defineNuxtConfig({
 	extends: ['github:idmarinas/nuxt-layers/docs-bundle#master', 'docus'],
-	docsBundle: {},
+	docsBundle: {
+		socials: {
+			x: 'https://x.com/idmarinas',
+			reddit: 'https://reddit.com/u/idmarinas',
+			paypal: 'https://www.paypal.me/idmarinas',
+			bitly: 'https://bit.ly/m/idmarinas',
+			githubsponsors: 'https://github.com/sponsors/idmarinas',
+			linkedin: 'https://linkedin.com/in/idmarinas',
+		},
+	},
 	$production: {
 		llms: {
 			domain: 'https://idmarinas.github.io/template-bundle'
