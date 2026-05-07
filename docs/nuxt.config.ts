937072d140f1adf4a4ce42759a239a6e06c1b922
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-07 17:48:56 +0200

    chore(docs): add libraries and support links to docsBundle configuration in nuxt.config.ts

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,6 +1,20 @@
 export default defineNuxtConfig({
 	extends: ['github:idmarinas/nuxt-layers/docs-bundle#master', 'docus'],
 	docsBundle: {
+		libraries: [
+			{
+				title: 'Symfony Components',
+				icon: 'i-tabler-brand-symfony',
+				to: 'https://www.symfony.com/components',
+				description: 'This project relies on these components for most of its features.'
+			},
+			{
+				title: 'Other component',
+				icon: 'i-tabler-components',
+				to: 'https://www.example.com',
+				description: 'Other Component used in this project.'
+			}
+		],
 		socials: {
 			x: 'https://x.com/idmarinas',
 			reddit: 'https://reddit.com/u/idmarinas',
@@ -8,6 +22,23 @@ export default defineNuxtConfig({
 			bitly: 'https://bit.ly/m/idmarinas',
 			githubsponsors: 'https://github.com/sponsors/idmarinas',
 			linkedin: 'https://linkedin.com/in/idmarinas',
+		},
+		support_links: {
+			title: 'Support me',
+			links: [
+				{
+					icon: 'i-tabler-brand-paypal',
+					label: 'PayPal.Me',
+					to: 'https://www.paypal.me/idmarinas',
+					target: '_blank'
+				},
+				{
+					icon: 'i-tabler-brand-github',
+					label: 'GitHub Sponsor',
+					to: 'https://github.com/sponsors/idmarinas',
+					target: '_blank'
+				}
+			]
 		},
 	},
 	$production: {
